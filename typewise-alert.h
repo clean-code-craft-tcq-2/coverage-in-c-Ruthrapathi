f6f680d15544e52b9bb4f7f2e2e4d0b287502601
Ruthrapathi <90901003+Ruthrapathi@users.noreply.github.com>
2022-03-11 11:12:38 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,4 +1,4 @@
-#pragma once
+#include <stdio.h>
 
 typedef enum {
   PASSIVE_COOLING,
@@ -9,15 +9,14 @@ typedef enum {
 typedef enum {
   NORMAL,
   TOO_LOW,
-  TOO_HIGH
+  TOO_HIGH,
+  BREACH_TYPE_LENGTH
 } BreachType;
-
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
-  TO_EMAIL
+  TO_EMAIL,
+  ALERT_TARGET_LENGTH
 } AlertTarget;
 
 typedef struct {
@@ -25,8 +24,21 @@ typedef struct {
   char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+const char Breachinfo[BREACH_TYPE_LENGTH][10] = {"Normal","Too Low","Too High"};
 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+typedef AlertTarget (*actionList) (BreachType breachType);
+
+int  passiveCoolingUpperLimit(CoolingType coolingType, int upperLimit);
+int  passiveCoolingLowerLimit(CoolingType coolingType, int lowerLimit);
+int  hiActiveCoolingUpperLimit(CoolingType coolingType, int upperLimit);
+int  hiActiveCoolingLowerLimit(CoolingType coolingType, int lowerLimit);
+int  medActiveCoolingUpperLimit(CoolingType coolingType, int upperLimit);
+int  medActiveCoolingLowerLimit(CoolingType coolingType, int lowerLimit);
+BreachType checkLowerLimit(double value, double lowerLimit);
+BreachType checkUpperLimit(double value, double upperLimit);
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+AlertTarget checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC, actionList *alertAction ); 
+AlertTarget sendToController(BreachType breachType);
+AlertTarget sendToEmail(BreachType breachType);
+AlertTarget  printAlert(char* alertInfo, AlertTarget target);
