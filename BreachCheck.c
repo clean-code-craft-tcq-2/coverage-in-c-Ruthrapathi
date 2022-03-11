11dc05acf7d26cce102017e22b9f4e16cadb5b00
Ruthrapathi <90901003+Ruthrapathi@users.noreply.github.com>
2022-03-11 11:55:00 +0530

    Update BreachCheck.c

--- BreachCheck.c
+++ BreachCheck.c
@@ -2,17 +2,18 @@
 
 BreachType checkLowerLimit(double value, double lowerLimit)
 {
-	if(value < lowerLimit) 
-	{
-    return TOO_LOW;
-	}
-	 return NORMAL;
+   if(value < lowerLimit) 
+   {
+      return TOO_LOW;
+   }
+ return NORMAL;
 }
+
 BreachType checkUpperLimit(double value, double upperLimit)
 {
-	if(value > upperLimit)
- 	{
-    return TOO_HIGH;
-    }
-	 return NORMAL;
+   if(value > upperLimit)
+   {
+      return TOO_HIGH;
+   }
+ return NORMAL;
 }
